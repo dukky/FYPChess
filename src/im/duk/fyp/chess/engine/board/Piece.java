4863b884ea9ee93ab95b73346d528f905612d9a8
dukky <andy.duks@gmail.com>
2015-11-05 00:23:40 +0000

    Added construtor to Piece class

--- src/im/duk/fyp/chess/engine/board/Piece.java
+++ src/im/duk/fyp/chess/engine/board/Piece.java
@@ -7,20 +7,30 @@ package im.duk.fyp.chess.engine.board;
  */
 public enum Piece {
 	
-	WHITE_PAWN,
-	WHITE_BISHOP,
-	WHITE_KNIGHT,
-	WHITE_ROOK,
-	WHITE_QUEEN,
-	WHITE_KING,
+	WHITE_PAWN(0),
+	WHITE_BISHOP(1),
+	WHITE_KNIGHT(2),
+	WHITE_ROOK(3),
+	WHITE_QUEEN(4),
+	WHITE_KING(5),
 	
-	BLACK_PAWN,
-	BLACK_BISHOP,
-	BLACK_KNIGHT,
-	BLACK_ROOK,
-	BLACK_QUEEN,
-	BLACK_KING,
+	BLACK_PAWN(6),
+	BLACK_BISHOP(7),
+	BLACK_KNIGHT(8),
+	BLACK_ROOK(9),
+	BLACK_QUEEN(10),
+	BLACK_KING(11),
 	
-	EMPTY
+	EMPTY(12);
+	
+	private int val;
+	
+	private Piece(int val) {
+		this.val = val;
+	}
+	
+	public int value() {
+		return val;
+	}
 	
 }
